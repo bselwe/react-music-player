a6928f23b98ad9f715c3e40dbe37b0e10bca4644
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2018-04-09 21:23:21 +0200

    Add redux-thunk

--- src/Store.ts
+++ src/Store.ts
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware, combineReducers, Store } from "redux";
 import { Provider } from "react-redux";
 import { handleActions } from "redux-actions";
+import thunk from "redux-thunk";
 
 const initialState: any = {};
 let store: Store<any>;
 const reducers = handleActions({}, initialState);
 
 store = createStore(
-    reducers
+    reducers,
+    applyMiddleware(thunk)
 );
 
 export default store;
