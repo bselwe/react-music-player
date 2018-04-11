990b1c7611741d6620ece617a710be08a4df9ea2
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2018-04-11 12:22:01 +0200

    Add song

--- src/Store.ts
+++ src/Store.ts
@@ -9,7 +9,8 @@ const initialState: AppState = {
     songs: [
         { id: "empty-bottles", name: "Empty bottles - Yelawolf", image: "https://i2.wp.com/truetoo.co/wp-content/uploads/2015/05/YELAWOLF-LS-COVER-DARK-PA-650x650.jpg?fit=500%2C300" },
         { id: "take-me-out", name: "Take me out - Franz Ferdinand", image: "http://i3.ytimg.com/vi/GhCXAiNz9Jo/maxresdefault.jpg" },
-        { id: "the-less-i-know-the-better", name: "The Less I Know The Better - Tame Impala", image: "https://www.jambase.com/wp-content/uploads/2015/11/tameimpalavideo-1480x832.jpg" }
+        { id: "the-less-i-know-the-better", name: "The Less I Know The Better - Tame Impala", image: "https://www.jambase.com/wp-content/uploads/2015/11/tameimpalavideo-1480x832.jpg" },
+        { id: "dna", name: "DNA - Kendrick Lamar", image: "https://www.vladtv.com/images/size_fs/video-226925.jpg" }
     ]
 };
 
