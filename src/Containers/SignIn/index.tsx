79d7f502804ac7c647b196e8c16395e2ab6ea5b1
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2018-06-03 21:22:57 +0200

    Stylize Login screen

--- src/Containers/SignIn/index.tsx
+++ src/Containers/SignIn/index.tsx
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import { withRouter, RouteComponentProps } from "react-router-native";
-import { View, Text, TouchableHighlight } from "react-native";
+import { View, Text, TouchableHighlight, TouchableWithoutFeedback, TouchableOpacity } from "react-native";
 import { connect, Dispatch } from "react-redux";
 import { SignInWithPassword } from "./reducers";
 import { styles } from "./styles";
 import * as routes from "../../Infrastructure/Navigation/Routes";
 import t from 'tcomb-form-native';
 
-// Form
 const Form = t.form.Form;
 
-// Form model
 const User = t.struct({
     email: t.String,
     password: t.String,
@@ -18,16 +16,12 @@ const User = t.struct({
 
 var options = {
     fields: {
-        email: {
-        label: 'User email' // <= label for the name field
-      },
-      password: {
-        label: 'Password', // <= label for the name field
-        secureTextEntry: true
-      }
+        password: {
+            secureTextEntry: true
+        }
     },
-    auto: 'placeholders'
-  };
+    auto: 'placeholders',
+};
 
 interface SignInStateProps {
     signInError?: string;
@@ -44,14 +38,16 @@ class SignIn extends React.Component<SignInProps> {
     render() {
         return <View style={styles.container}>
             <Text style={styles.title}>Login</Text>
-            <Form ref="form" type={User} options={options}/>
+            <Form ref="form" type={User} options={options} />
             <View>
                 <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
                     <Text style={styles.buttonText}>Login</Text>
                 </TouchableHighlight>
-                <TouchableHighlight style={styles.button} onPress={() => this.props.navigateToSignUp()} underlayColor='#99d9f4'>
-                    <Text style={styles.buttonText}>Sign up</Text>
-                </TouchableHighlight>
+                <TouchableOpacity onPress={() => this.props.navigateToSignUp()}>
+                    <View>
+                        <Text style={styles.buttonText}>Sign up</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         </View>;
     }
--- src/Containers/SignIn/styles.ts
+++ src/Containers/SignIn/styles.ts
@@ -1,46 +1,69 @@
 import { StyleSheet } from "react-native";
+import t from 'tcomb-form-native';
+
+const stylesheet = t.form.Form.stylesheet;
+
+stylesheet.textbox.normal.borderWidth = 0;
+stylesheet.textbox.error.borderWidth = 0;
+stylesheet.textbox.normal.marginBottom = 0;
+stylesheet.textbox.error.marginBottom = 0;
+
+stylesheet.textboxView.normal.borderWidth = 0;
+stylesheet.textboxView.error.borderWidth = 0;
+stylesheet.textboxView.normal.borderRadius = 0;
+stylesheet.textboxView.error.borderRadius = 0;
+stylesheet.textboxView.normal.borderBottomWidth = 1;
+stylesheet.textboxView.error.borderBottomWidth = 1;
+stylesheet.textbox.normal.marginBottom = 5;
+stylesheet.textbox.error.marginBottom = 5;
+
+stylesheet.textbox.normal.fontSize = 20;
 
 export const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "center",
-        backgroundColor: "rgba(52, 52, 52, 0.5)",
-        padding: 20
-    },
-    // title: {
-    //     textAlign: "center",
-    //     paddingTop: 24,
-    //     fontSize: 24,
-    //     fontWeight: "bold",
-    //     color: "white"
-    // }
-    // container: {
-    //     justifyContent: 'center',
-    //     marginTop: 50,
-    //     padding: 20,
-    //     backgroundColor: "rgba(52, 52, 52, 0.5)",
-    //   },
-      title: {
-        fontSize: 30,
-        alignSelf: 'center',
-        marginBottom: 30,
-        color: "white"
-      },
-      buttonText: {
-        fontSize: 18,
-        color: 'white',
-        alignSelf: 'center'
-      },
-      button: {
-        height: 36,
-        backgroundColor: '#48BBEC',
-        borderColor: '#48BBEC',
-        borderWidth: 1,
-        borderRadius: 8,
-        marginBottom: 10,
-        alignSelf: 'stretch',
-        justifyContent: 'center'
-      }
+  container: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor: "rgba(52, 52, 52, 0.7)",
+    padding: 20
+  },
+  formContainer: {
+    backgroundColor: "white"
+  },
+  // title: {
+  //     textAlign: "center",
+  //     paddingTop: 24,
+  //     fontSize: 24,
+  //     fontWeight: "bold",
+  //     color: "white"
+  // }
+  // container: {
+  //     justifyContent: 'center',
+  //     marginTop: 50,
+  //     padding: 20,
+  //     backgroundColor: "rgba(52, 52, 52, 0.5)",
+  //   },
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 30,
+    color: "white"
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    marginTop: 20,
+    height: 36,
+    width: "40%",
+    backgroundColor: '#ffb74d',
+    borderWidth: 0,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'center',
+    justifyContent: 'center'
+  }
 });
