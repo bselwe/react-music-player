a85272c35d258e3486ce9002ae249ccf818e966b
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2018-06-06 02:37:14 +0200

    Add UpdateFavorite action

--- src/Containers/SongScreen/reducers.ts
+++ src/Containers/SongScreen/reducers.ts
@@ -1,3 +1,7 @@
+import { FetchFavorites } from './../FavoritesScreen/reducers';
+import { Artist } from './../../Infrastructure/Navigation/Routes';
+import { apiClient } from './../../Services/ApiClient';
+import { Dispatch } from 'redux';
 import { ReducerMap, createAction } from "redux-actions";
 import { addReducerFactory } from "../../Utils/AddReducer";
 import { Thunk } from "../../Utils/Thunk";
@@ -16,8 +20,32 @@ addReducer(ToggleSong,
     }
 );
 
-export const UpdateFavorite = createAction("SONG/UPDATE_FAVORITE", (isFavorite: boolean) => ({ isFavorite }));
-addReducer(UpdateFavorite,
+export const UpdateFavorite = (isFavorite: boolean) =>
+    async (dispatch: Dispatch<any>, getState: () => AppState) => {
+        const currentSong = getState().currentSong;
+        if (currentSong === undefined)
+            return;
+
+        try {
+            let response: Models.ApiResponse<{}, {}>;
+            if (isFavorite)
+                response = await apiClient.addFavourite({ Id: currentSong.id, Title: currentSong.title, Artist: currentSong.artist.name, Photo: currentSong.album.cover });
+            else 
+                response = await apiClient.removeFavourite({ Id: currentSong.id });
+            
+            if (response.IsSuccess) {
+                dispatch(UpdateFavoriteStatus(isFavorite));
+                dispatch(FetchFavorites());
+            }
+        }
+        catch {
+            // Error
+        }
+    };
+
+
+export const UpdateFavoriteStatus = createAction("SONG/UPDATE_FAVORITE_STATUS", (isFavorite: boolean) => ({ isFavorite }));
+addReducer(UpdateFavoriteStatus,
     (state, action) => {
         return {
             ...state,
